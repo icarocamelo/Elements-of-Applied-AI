7517e58dd057af6203dd5150a0196ed4e5e94d78
Icaro Camelo <icarorvc@gmail.com>
2022-01-26 21:50:48 -0500

    tutorial 3

---
+++ src/tutorial3.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# Example 1
+data = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',header=None)
+data.columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class']
+
+data.head()
+
+# Example 2
+from pandas.api.types import is_numeric_dtype
+
+for col in data.columns:
+    if is_numeric_dtype(data[col]):
+        print('%s:' % (col))
+        print('\t Mean = %.2f' % data[col].mean())
+        print('\t Standard deviation = %.2f' % data[col].std())
+        print('\t Minimum = %.2f' % data[col].min())
+        print('\t Maximum = %.2f' % data[col].max())
+
+
